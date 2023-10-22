ca5d92c8298861722c2676b5c48bc723efe3b5f6
isthistaken3 <148587538+isthistaken3@users.noreply.github.com>
2023-10-22 10:54:46 -0400

    Create emily2024.js

---
+++ emily2024.js
@@ -0,0 +1,12 @@
+<script>
+function myFunction() {
+  let text;
+  let person = prompt("Please enter your name:", "Harry Potter");
+  if (person == null || person == "") {
+    text = "User cancelled the prompt.";
+  } else {
+    text = "Hello " + person + "! How are you today?";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+</script>
